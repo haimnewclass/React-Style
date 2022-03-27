e76ab2bf41211e789f8896eaf2b75fc936deb54e
haimnewclass <56435506+haimnewclass@users.noreply.github.com>
2022-03-27 11:18:50 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+import './my.scss';
 function App() {
+  const [state,setState] = useState('aaa');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div style={{display:'block',width:'200px',backgroundColor:'red'}}>
+       Btn
+     </div>
+     {
+       //<div class="aaa ccc"   className={`${state} ccc`} onClick={()=>{ 
+     }
+     <div className={`${state} ccc`} onClick={()=>{
+       if(state==='aaa')
+          setState('bbb');
+        else
+          setState('aaa');
+
+     }}>
+       Btn2
+     </div>
+     <div className='ddd'>
+      Click
+     </div>
+     <div className='ggg'>
+      Click
+     </div>
     </div>
   );
 }
